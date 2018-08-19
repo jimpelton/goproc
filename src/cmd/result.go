40e38cd2c5388cb6948af28cfda6e0e8a3ee2edc
Jim Pelton <jimpelton@blacksagetech.com>
2018-08-19 12:10:01 -0600

    initial commit

---
+++ src/cmd/result.go
@@ -0,0 +1,19 @@
+package main
+
+type Result struct {
+	r chan interface{}
+}
+
+func NewResult() *Result {
+	return &Result{
+		r: make(chan interface{}, 1),
+	}
+}
+
+func (r *Result) Done(v interface{}) {
+	r.r <- v
+}
+
+func (r *Result) Wait() interface{} {
+	return <-r.r
+}
